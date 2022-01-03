cc4a3d11ed852f47c7cc76babac49b86f4d55735
hernandoponzon <hernandonunez13@gmail.com>
2022-01-03 13:43:04 -0500

    Se añadieron ejemplos de usos de el scaffold

--- lib/pages/pagina_home.dart
+++ lib/pages/pagina_home.dart
@@ -6,14 +6,27 @@ class PaginaHome extends StatelessWidget{
   Widget build(BuildContext context) {
     
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Text("Titulo de appbar"),
+      ),
       floatingActionButton: FloatingActionButton(
-        onPressed: (){
-
-        },
+        onPressed: (){ },
         child: Icon(Icons.check),
       ),
-      body: Text("Hola Mundo"),
+      body: Center(
+        child: Text("Hola Mundo"),
+      ),
+      endDrawer: Drawer(),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.chat), label: "Chat"),
+          BottomNavigationBarItem(icon: Icon(Icons.notifications_active_rounded), label: "Notificatiuons")
+
+
+        ],
+        ),
+        backgroundColor: Colors.red.shade50,
     );
   }
   
